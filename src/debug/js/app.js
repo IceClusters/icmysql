3f2ad61a14c73b64e33b8b9ce4776aa345b37c27
Daniel GP <96537843+DaniGP17@users.noreply.github.com>
2023-08-16 01:30:20 +0200

    Setup stats, open/close modal and fix input text

--- src/debug/js/app.js
+++ src/debug/js/app.js
@@ -2,14 +2,170 @@ var isOpen = false;
 
 var resources = [];
 
+var queries = [
+    {
+        resourceName: "ice_gym",
+        type: "query",
+        db: "1",
+        query: "SELECT * FROM `gym` WHERE `id` = @id ",
+        values: { id: 1 },
+        time: 0.512,
+        currentTimestamp: Date.now(),
+        cache: true
+    },
+    {
+        resourceName: "ice_gym",
+        type: "query",
+        db: "1",
+        query: "SELECT * FROM `gym` WHERE `id` = @id ",
+        values: { id: 1 },
+        time: 0.512,
+        currentTimestamp: Date.now(),
+        cache: true
+    },
+    {
+        resourceName: "ice_gym",
+        type: "query",
+        db: "1",
+        query: "SELECT * FROM `gym` WHERE `id` = @id ",
+        values: { id: 1 },
+        time: 0.512,
+        currentTimestamp: Date.now(),
+        cache: true
+    },
+    {
+        resourceName: "ice_gym",
+        type: "query",
+        db: "1",
+        query: "SELECT * FROM `gym` WHERE `id` = @id ",
+        values: { id: 1 },
+        time: 0.512,
+        currentTimestamp: Date.now(),
+        cache: true
+    },
+    {
+        resourceName: "ice_gym",
+        type: "query",
+        db: "1",
+        query: "SELECT * FROM `gym` WHERE `id` = @id ",
+        values: { id: 1 },
+        time: 0.512,
+        currentTimestamp: Date.now(),
+        cache: true
+    },
+    {
+        resourceName: "ice_gym",
+        type: "query",
+        db: "1",
+        query: "SELECT * FROM `gym` WHERE `id` = @id ",
+        values: { id: 1 },
+        time: 0.512,
+        currentTimestamp: Date.now(),
+        cache: true
+    },
+    {
+        resourceName: "ice_gym",
+        type: "query",
+        db: "1",
+        query: "SELECT * FROM `gym` WHERE `id` = @id ",
+        values: { id: 1 },
+        time: 0.512,
+        currentTimestamp: Date.now(),
+        cache: true
+    },
+    {
+        resourceName: "ice_gym",
+        type: "query",
+        db: "1",
+        query: "SELECT * FROM `gym` WHERE `id` = @id ",
+        values: { id: 1 },
+        time: 0.512,
+        currentTimestamp: Date.now(),
+        cache: true
+    },
+    {
+        resourceName: "ice_gym",
+        type: "query",
+        db: "1",
+        query: "SELECT * FROM `gym` WHERE `id` = @id ",
+        values: { id: 1 },
+        time: 0.512,
+        currentTimestamp: Date.now(),
+        cache: true
+    },
+    {
+        resourceName: "ice_gym",
+        type: "query",
+        db: "1",
+        query: "SELECT * FROM `gym` WHERE `id` = @id ",
+        values: { id: 1 },
+        time: 0.512,
+        currentTimestamp: Date.now(),
+        cache: true
+    },
+    {
+        resourceName: "ice_gym",
+        type: "query",
+        db: "1",
+        query: "SELECT * FROM `gym` WHERE `id` = @id ",
+        values: { id: 1 },
+        time: 0.512,
+        currentTimestamp: Date.now(),
+        cache: true
+    }
+]
+
+function LoadStats() {
+    var data = {
+        queries: 150,
+        slowQueries: 10,
+        timeQueries: 0.74,
+        timeQuerying: 9.28,
+        scripts: 10,
+        databasesUsed: 2,
+        failedQueries: 1
+    }
+    $("#statsQueries").html("Queries: " + data.queries);
+    $("#statsSlowQueries").html("Slow queries: " + data.slowQueries);
+    $("#statsAverageQueries").html("Average queries: " + data.timeQueries + "ms");
+    $("#statsTimeQuerying").html("Time querying: " + data.timeQuerying + " s");
+    $("#statsScripts").html("Scripts: " + data.scripts);
+    $("#statsDBCount").html("Databases used: " + data.databasesUsed);
+    $("#statsFailedQueries").html("Failed queries: " + data.failedQueries);
+}
+
+function CloseResourceModal() {
+    $("#resourceModal").removeClass("--active");
+    $("#resourceModal").addClass("--hide");
+    setTimeout(() => {
+        $("#resourceModal").removeClass("--hide");
+        $("#resourceModal").css("display", "none");
+    }, 500);
+}
+
 function OpenResource(resourceName) {
     if (!resources.includes(resourceName)) return;
-    console.log("Opening resource " + resourceName);
+    $("#resourceModal").css("display", "");
+    $("#resourceModal").addClass("--active");
+    $("#resourceModalTitle").html(resourceName);
+    var resourceQueries = queries.filter(query => query.resourceName == resourceName);
+    console.log("resourceQueries: ", resourceQueries);
+    $("#resourceModalBody").html("");
+    resourceQueries.forEach(query => {
+        $("#resourceModalBody").append(`
+            <tr>
+                <td id="dbid">${query.cache ? '<i class="fa-solid fa-memory"></i> ' : ''}${query.db}</td>
+                <td id="query">${query.query}</td>
+                <td id="values">${JSON.stringify(query.values)}</td>
+                <td id="time">${query.time} ms</td>
+            </tr>
+        `)
+    });
 }
 
 function ListenSearchInput() {
-    $("#search-resource-input").on("input", () => {
-        const input = $("#search-resource-input").val();
+    $("#inputBox").on("input", () => {
+        const input = $("#inputBox").val();
         if (input.length < 0) return;
         const filtered = resources.filter(resource => resource.toLowerCase().includes(input.toLowerCase()));
         $("#resources-content").html("");
@@ -74,10 +230,11 @@ function OpenUI(state) {
     isOpen = state;
 }
 
-LoadResources(["ice_gym", "ice_core", "ice_hud", "ice_callbacks", "ice_vehicleshop"]);
+LoadResources(["ice_gym", "ice_core", "ice_hud", "ice_callbacks", "ice_vehicleshop", "ice_asd", "ice_asd", "ice_asd", "ice_asd", "ice_asd", "ice_asd"]);
 ListenSearchInput();
 
 $(document).ready(function () {
+    LoadStats();
     window.addEventListener("message", function (event) {
         if (event.data.action == undefined) return;
         switch (event.data.action) {
