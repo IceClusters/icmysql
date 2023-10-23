9d1715f66d67e8540c8c381ef2f96939e17f2e1e
daniel <dani.dcshop.gg@gmail.com>
2023-10-23 22:44:39 +0200

    Add explation to metabuild existence

--- src/build.js
+++ src/build.js
@@ -5,6 +5,8 @@ const { build } = require('esbuild');
 if (!fs.existsSync('dist')) {
     fs.mkdirSync('dist');
 }
+
+// The metabuild.js is for know if the file exist and know if the server is using a compiled version, we create a new file and don't find the build.js to avoid to load a full build in the server memory
 fs.writeFileSync('dist/metabuild.js', '//DONT REMOVE THIS FILE', 'utf-8');
 build({
     entryPoints: ['./src/index.js'],
