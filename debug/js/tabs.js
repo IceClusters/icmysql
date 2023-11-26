01d75213468dbc9e2cc84c14d9a04f47ca334723
zKXDEX <deiividdlk@gmail.com>
2023-11-26 05:23:23 +0100

    added intercept tabs and improved home interface

---
+++ debug/js/tabs.js
@@ -0,0 +1,25 @@
+import { Tablist } from "https://cdn.jsdelivr.net/npm/jolty/dist/jolty.esm.min.js";
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    Tablist.data("navigation", (tablist) => {
+        const marker = tablist.firstElementChild;
+    
+        const setMarker = (tab) => {
+        marker.style.width = tab.offsetWidth + "px";
+        marker.style.translate = tab.offsetLeft + "px";
+        };
+    
+        return {
+        data: "ui-tabs",
+        on: {
+            show(instance, { tab }) {
+            setMarker(tab);
+            }
+        }
+        };
+    });
+    
+    Tablist.initAll();
+});
